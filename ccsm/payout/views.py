23f5e93392f2e54409adfc1f4e45551c2227ba11
Caleb Lee <28caleblee@gmail.com>
2018-12-09 13:37:46 -0800

    Added firebase/ServiceAccountKey.json

--- ccsm/payout/views.py
+++ ccsm/payout/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+
+# Fetch the service account key JSON file contents
+# Note follow this flow to keep our files interchangeable
+# Ex all you would have to change is  /jackirish/
+cred = credentials.Certificate('/Users/caleblee/Desktop/CCServer/ccsm/payout/ServiceAccountKey.json')
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL' : 'https://clickerclash.firebaseio.com/'
+})
+
+ref = db.reference()
+
 
 # Create your views here.
+# Works
+endtime = ref.child('tournaments').child('standard').child('st1').child('endDate').get()
+print(endtime)
